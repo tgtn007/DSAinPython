f6a576b8677e789ad6455a9a8f33918934bd6de9
Arpit <arpitfalcon1@gmail.com>
2020-05-03 21:37:56 +0530

    Completed first problem.

--- test.py
+++ test.py
@@ -1,36 +1,8 @@
-top = "qqwertyuiopp"
-mid = "aasdfghjkl;;"
-bot = "zzxcvbnm,.//"
+item = dict()
+print("Current Dict is: ", item)
 
-shift = input()
-s = input()
-for i, char in enumerate(s):
-    if shift == 'R':
-        if char in top:
-            index = top.find(char, 1)
-            char = top[index - 1]
-            print(char,end="")
-        if char in mid:
-            index = mid.find(char, 1)
-            char = mid[index - 1]
-            print(char, end="")
-        if char in bot:
-            index = bot.find(char, 1)
-            char = bot[index - 1]
-            print(char, end="")
-    else:
-        if char in top:
-            index = top.find(char, 1)
-            char = top[index + 1]
-            print(char, end="")
-        if char in mid:
-            index = mid.find(char, 1)
-            char = mid[index + 1]
-            print(char, end="")
-        if char in bot:
-            index = bot.find(char, 1)
-            char = bot[index + 1]
-            print(char, end="")
-
-# print(s)
-
+# using the subscript notation 
+# Dictionary_Name[New_Key_Name] = New_Key_Value 
+item['key2'] = 'for'
+item['key3'] = 'geeks'
+print("Updated Dict is: ", item)
