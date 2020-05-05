c3e43768bc40448c8a3ce20f69daea53475bb33a
Arpit <arpitfalcon1@gmail.com>
2020-05-05 07:38:03 +0530

    Reorganizing files.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Udacity NanoDegree Data Structures &amp; Algorithms projects
+
+Sets of problems on Data Structures & Algorithms in Python including:
+- Searching and sorting algorithms on different data structures
+- Evaluation of the efficiency of algorithms, time and space complexity
+- Advanced algorithms such as brute-force greedy algorithms, graph algorithms, and dynamic programming 
+
+
+Description of Problems
+------------
+
+**Problem 1:**
+Deconstruction of a series of open-ended problems into smaller components (e.g, inputs, outputs, series of functions).
+
+**Problem 2:**
+Open-ended practice problems: LRU Cache, Private Block-chain, File Recursion.
+
+**Problem 3:**
+Real-world open-ended problems: request routing for a web server, search-term auto-completion and Fibonacci heap
+
+**Problem 4:**
+Route-planning algorithm to calculate the shortest path between two points on a map with the implementation of the A* algorithm to find shortest path.
--- test.py
+++ test.py
@@ -1,6 +1,99 @@
-faces = {"Tetrahedron" : 4, "Cube" : 6, "Octahedron" : 8, "Dodecahedron": 12, "Icosahedron": 20}
-ans = 0
-for _ in range(int(input())):
-    s = input()
-    ans += faces[s]
-print(ans)
+import hashlib
+import datetime
+
+
+class Block:
+
+    def __init__(self, data, previous_hash):
+        self.timestamp = str(datetime.datetime.utcnow())
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash(self.timestamp, self.data, self.previous_hash)
+        self.next = None
+
+    def calc_hash(self, timestamp, data, previous_hash):
+        block_content = timestamp + str(data) + str(previous_hash)
+        sha = hashlib.sha256()
+        hash_str = block_content.encode('utf-8')
+
+        sha.update(hash_str)
+        return sha.hexdigest()[-10:]
+
+    def get_hash(self):
+        return self.hash
+
+    def get_previous_hash(self):
+        return self.previous_hash
+
+
+class Blockchain():
+
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def add_block(self, data, previous_hash):
+        if data == None:
+            return None
+        new_block = Block(data, previous_hash)
+        if self.head == None:
+            self.head = new_block
+            self.tail = self.head
+        else:
+            self.tail.next = new_block
+            self.tail = new_block
+        return new_block
+
+    def get_last_hash(self):
+        if self.tail == None:
+            return '00000'
+        return self.tail.get_hash()
+
+    def is_chain_valid(self):
+        node = self.head
+        previous_hash = '00000'
+        while node:
+            if node.get_previous_hash() != previous_hash:
+                return False
+            previous_hash = node.get_hash()
+            node = node.next
+        return True
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield ['Data :{}, SHA256 Hash: {}, Previous Hash: {}, Timestamp: {}'.format(node.data, node.hash,
+                                                                                        node.previous_hash,
+                                                                                        node.timestamp)]
+            node = node.next
+
+    def __repr__(self):
+        return str([v for v in self])
+
+
+def test(newblocks):
+    blockchaintest = Blockchain()
+    for blockdata in newblocks:
+        previous_hash = blockchaintest.get_last_hash()
+        new_block = blockchaintest.add_block(blockdata, previous_hash)
+        if new_block != None:
+            print("Adding new block: {}".format(new_block.data))
+            print("TimeStamp: {}".format(new_block.timestamp))
+            print("Hash: {}".format(new_block.hash))
+            print("Previous Hash: {}".format(previous_hash))
+            print("\n")
+        else:
+            print("Block with value: {} not added".format(blockdata))
+            print("\n")
+    if blockchaintest.is_chain_valid():
+        print('Pass: blockchain is Valid')
+    else:
+        print('Fail: blockchain is not Valid')
+    print(blockchaintest)
+
+
+test([])  # Expected output: Pass: blockchain is Valid
+test([100000])  # Expected output: Pass: blockchain is Valid
+test([10, 100, 100, 80, '', 2])  # Expected output: Pass: blockchain is Valid
+test([100000, 20, 45, 80, None, 2])  # Expected output: Pass: blockchain is Valid
+test([10, 20, 45, 80, 2])  # Expected output: Pass: blockchain is Valid
